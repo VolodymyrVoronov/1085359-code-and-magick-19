12668c97bdba5ce05e45fe33222a672d3c66238c
Volodymyr <malones.vladimir@gmail.com>
2020-01-23 21:43:24 +0100

    fix position of bars

--- js/stat.js
+++ js/stat.js
@@ -2,14 +2,14 @@
 
 var RESULTS_WIDTH = 420;
 var RESULTS_HEIGHT = 270;
-var RESULTS_POSITION_X = 100;
+var RESULTS_POSITION_X = 110;
 var RESULTS_POSITION_Y = 10;
 var GAP = 10;
 var FONT_GAP = 250;
 var FONT_GAP_Y = 100;
 var FONT_GAP_X = 50;
 var BAR_WIDTH = 40;
-var BAR_POSITON_Y = 90;
+var BAR_POSITON_Y = 230;
 var BAR_GAP = 50;
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CLOUD_COLOR = 'rgba(255, 255, 255, 1)';
@@ -70,11 +70,11 @@ window.renderStatistics = function (ctx, players, times) {
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(Math.floor(times[i]), FONT_GAP_X = FONT_GAP_X + 100, FONT_GAP_Y - 20);
+    ctx.fillText(Math.floor(times[i]), FONT_GAP_X = FONT_GAP_X + 100, FONT_GAP_Y - 15);
     paintingPlayer(players, ctx, i);
     // ctx.rotate( (Math.PI / 180) * 25);
     // generateBar(ctx, BAR_GAP, MULTIPLIER, BAR_POSITON_Y, BAR_WIDTH, times, maxTime, i);
-    ctx.fillRect(BAR_GAP = BAR_GAP + 100, BAR_POSITON_Y, BAR_WIDTH, (Math.floor(times[i]) / (maxTime / 100)));
+    ctx.fillRect(BAR_GAP = BAR_GAP + 100, BAR_POSITON_Y, BAR_WIDTH, -(Math.floor(times[i]) / (maxTime / 100)));
     // console.log(Math.floor(times[i]) / (maxTime / 100));
   }
 
